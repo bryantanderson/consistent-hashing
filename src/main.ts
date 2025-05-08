8c4bd483585225958eca2a541bf0d97a9f913846
Bryant <andersonciputra@gmail.com>
2025-05-09 09:28:11 +1000

    chore: add README

--- src/main.ts
+++ src/main.ts
@@ -14,13 +14,26 @@ async function main() {
 		terminal: false,
 	});
 
+  // TODO: Rebalancing of the values when a node is added / removed
+  //
+  // If a node is added, values that are in the range of the subsequent clockwise node
+  // but now belong to the new node should be re-assigned to the new node
+  //
+  // If a node is removed, values that are in the range of the removed node but now belong to
+  // the subsequent clockwise node should be re-assigned to the new node
+
 	rl.on("line", (line) => {
 		// TODO: Consistent hashing logic. Hashed value should be assigned to the next Redis node
 		// in the clockwise direction
 		console.log(`Received: ${line}`);
+
+    if (line === 'PRINT_HASH_RING') {
+      // TODO: Log the hash ring to stdout
+    }
+
 		const hash = generateMD5Hash(line);
 		console.log(`Hash: ${hash}`);
-		// TODO: Log the hash ring to stdout on an interval / when a specific command is provided in stdin
+		// TODO(maybe): Log the hash ring to stdout on an interval
 	});
 
 	rl.on("close", () => {
