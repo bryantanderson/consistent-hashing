4bb3037efd14b8adcde7c570e0e8a395a774d8e6
Bryant <andersonciputra@gmail.com>
2025-05-09 09:19:39 +1000

    feat: read line by line from stdin

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,32 @@
 import * as crypto from "crypto";
+import * as readline from "readline";
 
 function generateMD5Hash(key: string) {
-  return crypto.createHash("md5").update(key).digest("base64");
+	return crypto.createHash("md5").update(key).digest("base64");
 }
 
 async function main() {
 	console.log("Main is running...");
+
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+		terminal: false,
+	});
+
+	rl.on("line", (line) => {
+		// TODO: Consistent hashing logic. Hashed value should be assigned to the next Redis node
+		// in the clockwise direction
+		console.log(`Received: ${line}`);
+		const hash = generateMD5Hash(line);
+		console.log(`Hash: ${hash}`);
+		// TODO: Log the hash ring to stdout on an interval / when a specific command is provided in stdin
+	});
+
+	rl.on("close", () => {
+		console.log("Input stream closed");
+		// TODO: Log the hash ring to stdout
+	});
 }
 
 main().catch((error) =>
